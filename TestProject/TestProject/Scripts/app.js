109fa0ce4c676dc85390db3e24f4f270c78d599e
Jay VanOrd <jvanord@indasysllc.com>
2018-11-15 09:24:29 -0500

    mocked loading with current route

--- TestProject/TestProject/Scripts/app.js
+++ TestProject/TestProject/Scripts/app.js
@@ -1,7 +1,36 @@
 ﻿; // Core App Features
 
+// Page View Model
 function pageViewModel(model) {
+
+	// Init
 	var self = this;
 	model = $.extend({ loading: true }, model); // defaults
+
+	// Properties
 	self.loading = ko.observable(!!model.loading);
+	self.path = ko.observable();
+
+	// Methods
+	self.loadCurrentPath = function () {
+		self.path(location.hash.substr(1));
+		self.loading(false);
+	};
 };
+
+// API Controller
+var Api = (function ($) {
+	var settings = {
+		baseUri: null
+	};
+	var me = {};
+
+	// Initialize
+	me.init = function (options) {
+		if (!$) throw new Error('jQuery failed to load!');
+		if (!options || !options.baseUri) throw new Error('Invalid API base URI!')
+		settings = $.extend(settings, options);
+	}
+
+	return me;
+})(window.jQuery);
