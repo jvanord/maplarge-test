f1b2d4d5e0e62cf6451c069066824d8139fef5ae
Jay VanOrd <jvanord@indasysllc.com>
2018-11-15 08:55:11 -0500

    initial loading state

---
+++ TestProject/TestProject/Scripts/app.js
@@ -0,0 +1,7 @@
+﻿; // Core App Features
+
+function pageViewModel(model) {
+	var self = this;
+	model = $.extend({ loading: true }, model); // defaults
+	self.loading = ko.observable(!!model.loading);
+};
